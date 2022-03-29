e1a16940825215279f2a03dd07d166f849335335
h3llohihi <71027676+Dnezzhello@users.noreply.github.com>
2022-03-29 15:52:29 +0700

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/* 1CS1 ສຸພະໄຊ ເນົາວະລາດ */
+// a program in C to read 10 numbers from keyboard and find their sum and  average
+
+void to_continue();
+
+int main() {
+	char choice;
+	
+	float arr[10], sum, i;
+
+	printf("\n--- The sum of 10 numbers from keyboard ---\n");
+	for (int i=0;i<10;i++) {
+		printf("number %d : ", i+1);
+		scanf(" %f", &arr[i]);
+		sum += arr[i];
+	}
+
+	printf("The sum is %.2f\n", sum);
+	printf("The average is %.2f", sum/10.0);
+	
+	to_continue();
+	return 0;
+}
+
+
+
+void to_continue() {
+	char choice;
+	printf("\nDo you want to continue? (y or n)\n");
+	printf("Input: ");
+	scanf(" %c", &choice);
+	if (choice == 'y') {
+		main();
+	} else {
+		printf("Good Bye!");
+	}
+}
+
+
+
