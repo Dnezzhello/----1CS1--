d27d5f17059106d1aa3d154eb66e131a342426cb
h3llohihi <71027676+Dnezzhello@users.noreply.github.com>
2022-04-26 22:26:17 +0700

    Update 62.c

--- 62.c
+++ 62.c
@@ -4,18 +4,24 @@
 /* 1CS1 ສຸພະໄຊ ເນົາວະລາດ */
 //Write a program in c to create a text file .write contents using fwrite and read from file using fread
 
-typedef struct Student {
+struct Student {
 	char full_name[40];
 	char std_id[5];
 	int age;
-} students;
+};
 
 int main() {
 
 	FILE *file;
-	int n;
+	int n, check, count = 1;
+	
 
-	file = fopen("test.txt", "wb");
+	file = fopen("test.dat", "w+");
+
+	if (file == NULL) {
+		printf("The file cannot either be created or opened properly\n");
+		exit(1);
+	}
 
 	printf("\n\n--- This programme is about creating a student's record ---\n\n");
 
@@ -23,23 +29,39 @@ int main() {
 	printf("Input: ");
 	scanf(" %d", &n);
 
-	students student[n];
+	struct Student student, s;
 
 	for (int i=0;i<n;i++) {
 		printf("\n-- STUDENT %d --\n", i+1);
+
+		fflush(stdin);
+
 		printf("Enter a student's full name: ");
-		scanf(" %[^\n]s", &student[i].full_name);
+		gets(student.full_name);
 		printf("Enter a student's id: ");
-		scanf(" %s", &student[i].std_id);
+		gets(student.std_id);
 		printf("Enter a student's age: ");
-		scanf(" %d", &student[i].age);
-		fwrite(&student, sizeof(student), 1, file);
+		scanf(" %d", &student.age);
+		check = fwrite(&student, sizeof(struct Student), 1, file);
 	}
 
-	
+	if (check == 1) {
+		printf("\nThe content was written succesfully\n");
+	} else {
+		printf("Error! Cannot write the content into the file\n");
+	}
 
+	//read file and show the content 
+	fseek(file, 0, SEEK_SET);
+	
+	while (fread(&s, sizeof(struct Student), 1, file)) {
+		printf("\n\n -- Student %d -- \n\n", count);
+		printf("Student's name: %s\n", s.full_name);
+		printf("Student's id: %s\n", s.std_id);
+		printf("Student's age: %d", s.age);
+		count++;
+	}
 
 	fclose(file);
 	return 0;
 }
-
